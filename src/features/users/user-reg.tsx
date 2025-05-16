6fa1b1d235c5798954692919d201f4009ee56f72
missymaloney <missymaloney1@gmail.com>
2025-05-15 21:16:04 -0400

    added post basic structure

--- src/features/users/user-reg.tsx
+++ src/features/users/user-reg.tsx
@@ -1,22 +1,49 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function UserRegistrationPage() {
-  const [data, setData] = useState('');
+const UserRegistrationPage: React.FC = () => {
+  //const [data, setData] = useState('');
 
-  useEffect(() => {
-    async function grabData() {
-      const response = await axios.get(
-        'http://localhost:3000/syc/users/68263e4fea86a5e3e602f793'
-      );
-      if (response.status === 200) {
-        console.log(response);
-        setData(response.data);
-      }
-    }
-    grabData();
-  }, []);
+  interface User {
+    name: string;
+    emailAddress: string;
+  }
 
-  return <>{JSON.stringify(data)}</>;
-}
+  const handlePostUser = () => {
+    const postObject: User = {
+      name: 'Becky Mancuso',
+      emailAddress: 'becky@example.com',
+    };
+
+    axios.post('http://localhost:3000/syc/users', postObject);
+  };
+
+  // useEffect(() => {
+  //   async function grabData() {
+  //     const response = await axios.get(
+  //       'http://localhost:3000/syc/users/68263e4fea86a5e3e602f793'
+  //     );
+  //     if (response.status === 200) {
+  //       console.log(response);
+  //       setData(response.data);
+  //     }
+  //   }
+  //   grabData();
+  // }, []);
+
+  //return <>{JSON.stringify(data)}</>;
+
+  return (
+    <div className="flex h-screen my-auto items-center">
+      <div className="m-auto">
+        <button
+          className="inline-block align-middle bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={handlePostUser}
+        >
+          Post User
+        </button>
+      </div>
+    </div>
+  );
+};
 export default UserRegistrationPage;
